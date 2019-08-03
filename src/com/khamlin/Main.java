3d6945e843453026e6bd3658f637c19de74a359e
khamlin <kevin.hamlin@gmail.com>
2019-08-02 20:17:18 -0400

    Updated with ComplexNumber input.

--- src/com/khamlin/Main.java
+++ src/com/khamlin/Main.java
@@ -3,15 +3,27 @@ package com.khamlin;
 public class Main {
 
     public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal()); // 2.0
+        System.out.println("one.imaginary= " + one.getImaginary());  // 2.0
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());  // -0.5
+        System.out.println("one.imaginary= " + one.getImaginary());  // 3.5
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());  // 3.0
+        System.out.println("number.imaginary= " + number.getImaginary());  // -5.0
 
-        Carpet carpet = new Carpet(3.5);
-        Floor floor = new Floor(2.75, 4.0);
-        Calculator calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
-        carpet = new Carpet(1.5);
-        floor = new Floor(5.4, 4.5);
-        calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
+
+//        Carpet carpet = new Carpet(3.5);
+//        Floor floor = new Floor(2.75, 4.0);
+//        Calculator calculator = new Calculator(floor, carpet);
+//        System.out.println("total= " + calculator.getTotalCost());
+//        carpet = new Carpet(1.5);
+//        floor = new Floor(5.4, 4.5);
+//        calculator = new Calculator(floor, carpet);
+//        System.out.println("total= " + calculator.getTotalCost());
 
 //        Point first = new Point(6,5);
 //        Point second = new Point(3,1);
