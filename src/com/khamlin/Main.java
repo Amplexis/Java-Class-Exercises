1d02140ca45121b3ca32545658f1e52078acd052
khamlin <kevin.hamlin@gmail.com>
2019-08-04 19:17:59 -0400

    Updated with calls to Cuboid and Rectangle.

--- src/com/khamlin/Main.java
+++ src/com/khamlin/Main.java
@@ -3,14 +3,26 @@ package com.khamlin;
 public class Main {
 
     public static void main(String[] args) {
-        Circle circle = new Circle(3.75);
-        System.out.println("circle.radius= " + circle.getRadius());  // 3.75
-        System.out.println("circle.area= " + circle.getArea());  // 44.17894699...
-        Cylinder cylinder = new Cylinder(5.55, 7.25);
-        System.out.println("cylinder.radius= " + cylinder.getRadius());  //5.55
-        System.out.println("cylinder.height= " + cylinder.getHeight());  // 7.25
-        System.out.println("cylinder.area= " + cylinder.getArea());  //96.76890771...
-        System.out.println("cylinder.volume= " + cylinder.getVolume());  //701.57458091....
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth()); // 5.0
+        System.out.println("rectangle.length= " + rectangle.getLength());  // 10.0
+        System.out.println("rectangle.area= " + rectangle.getArea());  // 50.0
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());  //  5.0
+        System.out.println("cuboid.length= " + cuboid.getLength());  // 10.0
+        System.out.println("cuboid.area= " + cuboid.getArea());  // 50.0
+        System.out.println("cuboid.height= " + cuboid.getHeight());  // 5.0
+        System.out.println("cuboid.volume= " + cuboid.getVolume());  // 250.0
+
+//        Circle circle = new Circle(3.75);
+//        System.out.println("circle.radius= " + circle.getRadius());  // 3.75
+//        System.out.println("circle.area= " + circle.getArea());  // 44.17894699...
+//        Cylinder cylinder = new Cylinder(5.55, 7.25);
+//        System.out.println("cylinder.radius= " + cylinder.getRadius());  //5.55
+//        System.out.println("cylinder.height= " + cylinder.getHeight());  // 7.25
+//        System.out.println("cylinder.area= " + cylinder.getArea());  //96.76890771...
+//        System.out.println("cylinder.volume= " + cylinder.getVolume());  //701.57458091....
 
 //        ComplexNumber one = new ComplexNumber(1.0, 1.0);
 //        ComplexNumber number = new ComplexNumber(2.5, -1.5);
