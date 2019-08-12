cbd2406918b8e99f9f3d287da9e89cb79ffe175b
Amplexis <kevin.hamlin@gmail.com>
2019-08-12 18:28:29 -0400

    Updated to add calls to Hamburger class

--- src/com/khamlin/Main.java
+++ src/com/khamlin/Main.java
@@ -4,16 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rectangle rectangle = new Rectangle(5, 10);
-        System.out.println("rectangle.width= " + rectangle.getWidth()); // 5.0
-        System.out.println("rectangle.length= " + rectangle.getLength());  // 10.0
-        System.out.println("rectangle.area= " + rectangle.getArea());  // 50.0
-        Cuboid cuboid = new Cuboid(5,10,5);
-        System.out.println("cuboid.width= " + cuboid.getWidth());  //  5.0
-        System.out.println("cuboid.length= " + cuboid.getLength());  // 10.0
-        System.out.println("cuboid.area= " + cuboid.getArea());  // 50.0
-        System.out.println("cuboid.height= " + cuboid.getHeight());  // 5.0
-        System.out.println("cuboid.volume= " + cuboid.getVolume());  // 250.0
+        Hamburger burger = new Hamburger("Keiser", true);
+        burger.addItem("Cheese", 0.37);
+        burger.addItem("Peppers", 0.25);
+        burger.addItem("Tomato", 0.71);
+        burger.addItem("Pickles", 0.21);
+        burger.addItem("Fries", 1.72);
+        burger.breakdown();
+
+//        Rectangle rectangle = new Rectangle(5, 10);
+//        System.out.println("rectangle.width= " + rectangle.getWidth()); // 5.0
+//        System.out.println("rectangle.length= " + rectangle.getLength());  // 10.0
+//        System.out.println("rectangle.area= " + rectangle.getArea());  // 50.0
+//        Cuboid cuboid = new Cuboid(5,10,5);
+//        System.out.println("cuboid.width= " + cuboid.getWidth());  //  5.0
+//        System.out.println("cuboid.length= " + cuboid.getLength());  // 10.0
+//        System.out.println("cuboid.area= " + cuboid.getArea());  // 50.0
+//        System.out.println("cuboid.height= " + cuboid.getHeight());  // 5.0
+//        System.out.println("cuboid.volume= " + cuboid.getVolume());  // 250.0
 
 //        Circle circle = new Circle(3.75);
 //        System.out.println("circle.radius= " + circle.getRadius());  // 3.75
